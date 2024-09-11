071795e26922680f85fdf8795731d7f57f0141aa
L.Trilos <l.triantafyllopoulos@astynomia.gr>
2024-09-11 13:52:23 +0300

    MInor cleanup

---
+++ src/main/vue-app/postcss.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  plugins: [
+    // https://github.com/postcss/autoprefixer
+    require('autoprefixer')({
+      overrideBrowserslist: ['Firefox >= 52', 'Chrome >= 54'],
+    }),
+
+    // https://github.com/elchininet/postcss-rtlcss
+    // If you want to support RTL css, then
+    // 1. yarn/npm install postcss-rtlcss
+    // 2. optionally set quasar.config.js > framework > lang to an RTL language
+    // 3. uncomment the following line:
+    // require('postcss-rtlcss')
+  ],
+};
