47b495afedaeb012b20ff472e29426644b228380
Lampros <lamprostriantafyllopoulos@yahoo.com>
2024-09-12 23:53:26 +0300

    Superset fully delegates login to Keycloak. Keycloak roles are mapped to Superset roles:   "admin": ["Admin"],   "superUser": ["Alpha"],   "simpleUser": ["Gamma"]
      Roles are also synced at every subsequent login

---
+++ superset/CustomSsoSecurityManager.py
@@ -0,0 +1,22 @@
+import logging
+from superset.security import SupersetSecurityManager
+
+# Configure logging (usually done once, not within a class)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+class CustomSsoSecurityManager(SupersetSecurityManager):
+    def oauth_user_info(self, provider, response=None):  # noqa: ARG002
+        me = self.appbuilder.sm.oauth_remotes[provider].get("openid-connect/userinfo")
+        me.raise_for_status()
+        data = me.json()
+        logging.info("User info from Keycloak: %s", data)
+        logging.info(data.get("realm_access", {}).get("roles", []))
+        return {
+            "username": data.get("preferred_username", ""),
+            "first_name": data.get("given_name", ""),
+            "last_name": data.get("family_name", ""),
+            "email": data.get("email", ""),
+            "role_keys": data.get("roles", []),
+            #"role_keys": data.get("realm_access", {}).get("roles", []),
+        }
